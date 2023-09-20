edccd107b362ef67980d0c5019ed855cd05ad153
Michael Bevilacqua <bev@michaelsmbp163.bc.edu>
2023-09-20 10:50:49 -0400

    First few steps

---
+++ PhyloTree-HuggErs_module3.R
@@ -0,0 +1,20 @@
+library(tidyverse) 
+library(ape)
+library(nlme)
+library(geiger)
+library(caper)
+library(phytools)
+library(viridis)
+library(MuMIn)
+
+anole <- read_csv("anole.dat.csv")
+anole.eco <- read_csv("anole.eco.csv")
+anole2 <- anole%>%
+  left_join(anole.eco)%>%
+  filter(!Ecomorph%in%c("U","CH"))%>%
+  na.omit()%>%
+  print()
+
+#1
+anole.log <- anole2%>%
+  mutate_at(c("SVL", "HTotal","PH","ArbPD"),log)
