9819c68cce19e5129142ba97774f5652384a6721
Michael Bevilacqua <bev@michaelsmbp163.bc.edu>
2023-09-22 10:32:04 -0400

    I know 2 is wrong because it produces residuals that are all 0

--- PhyloTree-HuggErs_module3.R
+++ PhyloTree-HuggErs_module3.R
@@ -18,5 +18,12 @@ anole2 <- anole%>%
 #1
 anole.log <- anole2%>%
   mutate_at(c("SVL", "HTotal","PH","ArbPD"),log)
+anole.log
 
 #2
+anole.lm1 <- lm(HTotal~ArbPD+Species,data=anole.log)
+anole.lm2 <- lm(HTotal~PH+Species,data=anole.log)
+
+#3
+anole.resid.1 <- resid(anole.lm1)
+anole.resid.2 <- resid(anole.lm2)
